80b1c0b2a04a0c44e663171993221560333b231e
Yann de The <ydethe@gmail.com>
2025-09-08 16:40:19 +0200

    Added _version.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,4 +10,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 <small>[Compare with latest](https://github.com/johncloud-paas/nixpack_template/compare/2d10686fdb6558db983e1bb11bcb15a48cf41f11...HEAD)</small>
 
+### Added
+
+- Added _version.py ([c951344](https://github.com/johncloud-paas/nixpack_template/commit/c951344b1454095fa8a784289afd822b8176441e) by Yann de The).
+
 <!-- insertion marker -->
--- src/nixpack_template/server.py
+++ src/nixpack_template/server.py
@@ -1,13 +1,15 @@
-# main.py
-from typing import Union
+from typing import Any, Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Any]:
+    # curl localhost:3000/items/1\?q=eee
     return {"item_id": item_id, "q": q}
