835b40b40866cd0f4879eab89722696a42bec9ca
Yann de The <ydethe@gmail.com>
2025-09-09 10:57:17 +0200

    Added links to coverage reports

---
+++ src/nixpack_template/__init__.py
@@ -0,0 +1,21 @@
+"""
+
+.. include:: ../../README.md
+
+# Testing
+
+## Run the tests
+
+To run tests, just run:
+
+    uv run pytest
+
+## Test reports
+
+[See test report](../tests/report.html)
+
+[See coverage](../coverage/index.html)
+
+.. include:: ../../CHANGELOG.md
+
+"""
